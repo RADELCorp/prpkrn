835c09de3552dd771f580160f6381df235308b5b
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-01-13 21:52:36 +0530

    33

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = '/prpkrn/';
+//$config['base_url'] = '/propkorner/';
 $config['base_url'] = 'http://propkorner.com/';
 
 /*
